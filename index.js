cbaf16ee65e21ea8da02c455d872e443caa78a95
Kevin Reid <kevinnivek@me.com>
2022-07-22 15:23:07 -0400

    adding employee

--- index.js
+++ index.js
@@ -5,14 +5,105 @@ const db = require('./db/connection');
 const app = express();
 
 
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const getEmployees = require('./routes/apiRoutes/employees'); 
+// importing functions
+const getEmployees = require('./routes/apiRoutes/employees.js') ;
 
-//importing functions to show data
-const showAllEmployees = require('./routes/apiRoutes/employees');
+
+
+
+const addNewEmployee = ((first_name, last_name, role_id, res) => {
+
+    const sql = `INSERT INTO employees (first_name, last_name, role_id)
+    VALUES (?,?,?)`
+
+    const params = [first_name, last_name, role_id];
+    console.log("the  params are:" + params);
+
+    db.query(sql, params, (err, result) => 
+        {
+            if (err) {
+                res.status(500).json({error: err.message});
+                return;
+            }
+    console.table(params)
+        });
+     showOrAddData();
+    });
+
+function getNewEmployeeInfo (){
+
+    return inquirer.prompt([
+
+        // Prompt User for information about new employee
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'What is their first name?',
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'What is the their last name?',
+        }, 
+        {
+            type: 'checkbox',
+            name: 'role',
+            message: 'What is their role?',
+            choices: ["Sales Lead", "Salesperson", "Software Engineer", "Account Manager", "Accountant", "Legal Team Lead", "Lawyer"],
+
+        },
+    ])
+        // Then checking the value of the input to call other functions
+    .then ( data => {
+        console.log(data);
+        const role = JSON.stringify(data.role);
+        let role_id = 0;
+        if (role === '["Sales Lead"]'){
+           role_id = 1;
+        } else if (role === '["Salesperson"]'){
+            role_id = 2;
+        } else if (role === '["Software Engineer"]'){
+            role_id = 3;
+        } else if (role === '["Account Manager"]'){
+            role_id = 4;
+        } else if (role === '["Accountant"]'){
+            role_id = 5;
+        } else if (role === '["Legal Team Lead"]'){
+            role_id = 6;
+        } else if (role === '["Lawyer"]'){
+            role_id = 7;
+        } 
+        console.log("The role_id is now" + role_id)
+        addNewEmployee(data.first_name, data.last_name, role_id); 
+    })
+}
+
+
+function validateChoice(choice) {
+    if(choice === '["View All Employees"]') {
+        console.log("You chose to show all employees");
+        getEmployees();
+        return;
+    }  else if (choice === '["Add Employee"]') {
+        console.log("You chose to add an employee");
+        getNewEmployeeInfo();
+        return;
+    } 
+    
+    
+    
+    else if (choice === '["Quit"]') {
+        console.log("Have a good day!");
+       return;
+    }
+
+}
+
 
 function showOrAddData () {
     return inquirer.prompt([
@@ -32,28 +123,9 @@ function showOrAddData () {
         // Then checking the value of the input to call other functions
     .then (data =>{
         const choice = JSON.stringify(data.choice);
+        validateChoice(choice)
 
-    // if the choice was "View All Employees" then query sql database for all employees
-    if(choice === '["View All Employees"]') {
-        console.log("You chose to show all employees");
-        let sql = `SELECT employees.id, employees.first_name, employees.last_name, roles.title AS title, department.name as department, roles.salary AS salary, employees.manager_id AS manager 
-                    FROM employees
-                    LEFT JOIN roles
-                    ON employees.role_id = roles.id
-                    LEFT JOIN department
-                    ON roles.department_id = department.id`;
-
-        db.query(sql, (err, res) => 
-        {
-            if (err) {
-                res.status(500).json({error: err.message});
-                return;
-            }
-        console.table(res);
-        
-        });
-        }
-        })
+    })
 }
 
 module.exports = showOrAddData;
--- routes/apiRoutes/employees.js
+++ routes/apiRoutes/employees.js
@@ -1,24 +1,27 @@
-const express = require('express');
-const router = express.Router();
+const showOrAddData = require('../..');
 const db = require('../../db/connection');
 
+function getEmployees () {
+
+    let sql = `SELECT employees.id, employees.first_name, employees.last_name, roles.title AS title, department.name as department, roles.salary AS salary, employees.manager_id AS manager 
+    FROM employees
+    LEFT JOIN roles
+    ON employees.role_id = roles.id
+    LEFT JOIN department
+    ON roles.department_id = department.id`;
+
+    db.query(sql, (err, res) => 
+        {
+            if (err) {
+                res.status(500).json({error: err.message});
+                return;
+            }
+        console.table(res);
+        
+        });
+        showOrAddData();
+    }
 
 
-router.get('/employees', (req, res) => {
 
-    const sql = `SELECT * FROM employees`;
-
-    db.query(sql, (err, rows) =>{
-        if (err) {
-            res.status(500).json({error: err.message});
-            return;
-        }
-    res.json({
-        message:'sucess',
-        data: rows
-    });
-});
-});
-
-
-module.exports = router;
+module.exports = getEmployees;
